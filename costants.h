177b9848830cbdb91a9a2df6d39822357ea54a00
Gabriele <gabri.cabal@gmail.com>
2021-02-25 11:39:16 +0100

    Implementate costanti per il progetto
    Implementate costanti per la versione, per semplificare l'utilizzo e per la dimensione della mappa

--- costants.h
+++ costants.h
@@ -3,7 +3,22 @@
 
 /**
  * Gabriele Cavallo
- * Contiene le costanti del
+ * Contiene le costanti del gioco
  */
+
+/**
+ * Versione del gioco
+ */
+#define GAME_VERSION "1.1"
+
+/**
+ * Dimensione della riga/colonna della mappa
+ */
+#define MAP_SIZE 20
+
+/**
+ * Stringa che rappresenta la fine della riga, in stile c++
+ */
+#define endl "\n"
 
 #endif //BATTAGLIANAVALE_COSTANTS_H
