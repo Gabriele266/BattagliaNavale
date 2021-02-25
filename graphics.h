ec9faeee9608f2a00228a27fdcde155afd938706
Gabriele <gabri.cabal@gmail.com>
2021-02-25 11:40:21 +0100

    Implementati commenti doxygen

--- graphics.h
+++ graphics.h
@@ -8,7 +8,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include "game_constr.h"
+#include "costants.h"
 
-void stampaVista(int dati[][]);
+/**
+ * @brief Scrive una vista di navi sul terminale
+ * @param dati Una matrice con la mappa del giocatore
+ * @param columns_count Numero di colonne della matrice
+ * @param row_count Numero di righe della matrice
+ */
+void stampaVista(Cell **dati, unsigned int row_count, unsigned int columns_count);
+
+/**
+ * Scrive l'introduzione del gioco sul terminale
+ */
+void printIntroduction();
 
 #endif //BATTAGLIANAVALE_GRAPHICS_H
